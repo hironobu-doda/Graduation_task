e02e6fc2249e976b1c2d7ad21967174d662b9e0f
hironobu_doda <ironnamono.rakusaku@gmail.com>
2019-04-26 13:39:57 +0900

    deviseの設定を行った

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  # resources :copings
+  #
+  # root 'users#index'
+  #
+  # resources :creation_dates, shallow: true do
+  #   resources :comments
+  # end
+  # # ログイン、アカウント編集後、任意のページに推移させるための記述
+  # devise_for :users, controllers: {
+  #         registrations: 'users/registrations'
+  # }
+  #
+  # resources :users, only: [:show, :index]
+
+
+# deviseが終わったら下を消して、上のコメントを消すこと
   resources :copings
 
   root 'users#index'
@@ -7,10 +24,6 @@ Rails.application.routes.draw do
   resources :creation_dates, shallow: true do
     resources :comments
   end
-  # ログイン、アカウント編集後、任意のページに推移させるための記述
-  devise_for :users, controllers: {
-          registrations: 'users/registrations'
-  }
 
   resources :users, only: [:show, :index]
 end
