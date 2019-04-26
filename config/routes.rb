c0749cfffcf39323124b84a3fcd7c6bcfac98312
hironobu_doda <ironnamono.rakusaku@gmail.com>
2019-04-26 12:21:42 +0900

    routes.rbを編集し,creation_dateのルーティングを作成した。また、この後作成するuser,devise,coping,comment機能のルーティングも先に入力しておいた。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  get 'creation_dates/index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :copings
+
+  root 'users#index'
+
+  resources :creation_dates, shallow: true do
+    resources :comments
+  end
+  # ログイン、アカウント編集後、任意のページに推移させるための記述
+  devise_for :users, controllers: {
+          registrations: 'users/registrations'
+  }
+
+  resources :users, only: [:show, :index]
 end
