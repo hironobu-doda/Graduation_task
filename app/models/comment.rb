3a85d956935dade2d73897e055525a322575b80f
hironobu_doda <ironnamono.rakusaku@gmail.com>
2019-04-26 19:05:30 +0900

    ストレス値が登録されていないとエラーになってしまう不具合を修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :creation_date
-  validates :before_content, presence: true
   belongs_to :user
+
+  validates :before_content, presence: true, length: { maximum: 200 }
+  validates :after_content, presence: true, length: { maximum: 200 }
+  validates :before_point, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
+  validates :after_point, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 end
--- app/models/coping.rb
+++ app/models/coping.rb
@@ -1,3 +1,4 @@
 class Coping < ApplicationRecord
   belongs_to :user
+  validates :coping_repertory, presence: true, length: { maximum: 200 }
 end
