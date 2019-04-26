b08b0be53a69ba09c245d313a26085042511cfae
hironobu_doda <ironnamono.rakusaku@gmail.com>
2019-04-26 13:55:53 +0900

    マイページを作成した

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/show'
 
-  devise_for :users
-  # resources :copings
-  #
-  # root 'users#index'
-  #
-  # resources :creation_dates, shallow: true do
-  #   resources :comments
-  # end
-  # # ログイン、アカウント編集後、任意のページに推移させるための記述
-  # devise_for :users, controllers: {
-  #         registrations: 'users/registrations'
-  # }
-  #
-  # resources :users, only: [:show, :index]
-
-
-# deviseが終わったら下を消して、上のコメントを消すこと
   resources :copings
 
   root 'users#index'
@@ -24,6 +8,22 @@ Rails.application.routes.draw do
   resources :creation_dates, shallow: true do
     resources :comments
   end
+  # ログイン、アカウント編集後、任意のページに推移させるための記述
+  devise_for :users, controllers: {
+          registrations: 'users/registrations'
+  }
 
   resources :users, only: [:show, :index]
+
+
+# deviseが終わったら下を消して、上のコメントを消すこと
+  # resources :copings
+  #
+  # root 'users#index'
+  #
+  # resources :creation_dates, shallow: true do
+  #   resources :comments
+  # end
+  #
+  # resources :users, only: [:show, :index]
 end
