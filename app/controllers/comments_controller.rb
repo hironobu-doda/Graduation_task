f2f9c50f6655eac6ae0840448fe5c77b964bb779
hironobu_doda <ironnamono.rakusaku@gmail.com>
2019-04-29 16:21:19 +0900

    comments_controller.rbのエラー文が長いのでprivate以下にまとめた

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,28 +8,7 @@ class CommentsController < ApplicationController
         # flash.discard
         format.js { render :index }
       else
-
-        if @comment.before_point.present?
-          unless @comment.before_point.between?(0, 100)
-            flash[:success] = '「現在のストレス度」は0~100の間で入力してください'
-          end
-        end
-
-        if @comment.after_point.present?
-          unless @comment.after_point.between?(0, 100)
-            flash[:success2] = '「改善後のストレス度」は0~100の間で入力してください'
-          end
-        end
-
-        unless @comment.before_point.present? && @comment.after_point.present?
-            flash[:success3] = 'ストレス度が空です'
-        end
-
-        unless @comment.before_content.present? && @comment.after_content.present?
-            flash[:success4] = 'コメントが空です'
-        end
-
-        flash[:success5] = '投稿できませんでした...'
+        error_message
         format.html { redirect_to creation_date_path(@creation_date) }
       end
     end
@@ -61,5 +40,29 @@ class CommentsController < ApplicationController
   private
   def comment_params
     params.require(:comment).permit(:creation_date_id, :before_content, :after_content, :before_point, :after_point, :date)
+  end
+
+  def error_message
+    if @comment.before_point.present?
+      unless @comment.before_point.between?(0, 100)
+        flash[:success] = '「現在のストレス度」は0~100の間で入力してください'
+      end
+    end
+
+    if @comment.after_point.present?
+      unless @comment.after_point.between?(0, 100)
+        flash[:success2] = '「改善後のストレス度」は0~100の間で入力してください'
+      end
+    end
+
+    unless @comment.before_point.present? && @comment.after_point.present?
+        flash[:success3] = 'ストレス度が空です'
+    end
+
+    unless @comment.before_content.present? && @comment.after_content.present?
+        flash[:success4] = 'コメントが空です'
+    end
+
+    flash[:success5] = '投稿できませんでした...'
   end
 end
