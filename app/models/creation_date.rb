a928e611c8301eed3f7892ec92bbab324d485538
hironobu_doda <ironnamono.rakusaku@gmail.com>
2019-04-29 12:29:46 +0900

    CreationDateモデルにdateとuser_idの2つに対してユニーク制約をかけた

--- app/models/creation_date.rb
+++ app/models/creation_date.rb
@@ -1,5 +1,6 @@
 class CreationDate < ApplicationRecord
-  validates :date, presence: true, uniqueness: true
+  # validates :date, presence: true, uniqueness: true
+  validates :date, presence: true, :uniqueness => {:scope => :user_id}
   has_many :comments, dependent: :destroy
   belongs_to :user
 end
